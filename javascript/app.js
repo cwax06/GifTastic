866b7e4184b4bfbb2fd86e9dd86a31a540546644
Andrew Jones <andrew@Andrews-MacBook-Pro.local>
2017-11-02 23:20:19 -0600

    homework walk

--- javascript/app.js
+++ javascript/app.js
@@ -1,14 +1,60 @@
+$(function () {
+    populateButtons(searchArray, 'searchButton', '#buttonsArea');
+    console.log("Page Loaded");
+})
 
+var searchArray = ['Dog', 'Cat', 'Bird'];
 
-var queryURL = "https://api.giphy.com/v1/gifs/" + animal + "?api_key=gM1pYFqpEaLhtsECJxi3YCmNMPhnNpWJ";
+function populateButtons(searchArray, classToAdd, AreaToAddTo) {
+    $(AreaToAddTo).empty();
+    for (var i = 0; i < searchArray.length; i++) {
+        var a = $('<button>');
+        a.addClass(classToAdd);
+        a.attr('data-type', searchArray[i]);
+        a.text(searchArray[i]);
+        $(AreaToAddTo).append(a);
+    }
+}
 
-var animal = "dog";
-// var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=40e9cece";
+$(document).on('click', '.searchButton', function () {
+    var type = $(this).data('type');
+    var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + type + '&api_key=gM1pYFqpEaLhtsECJxi3YCmNMPhnNpWJ&limit=5';
+    $.ajax({ url: queryURL, method: 'GET' })
+        .done(function (response) {
+            console.log(response);
+            for (var i = 0; i < response.data.length; i++) {
+                var searchDiv = $('<div class="search-item">');
+                var rating = response.data[i].rating;
+                var p = $('<p>').text('Rating: ' + rating);
+                var animated = response.data[i].images.fixed_height.url;
+                var still = response.data[i].images.fixed_height.still.url;
+                var image = $('<img>');
+                image.attr('src', still);
+                image.attr('data-still', still);
+                image.attr('data-animated', animated);
+                image.attr('data-state', 'still');
+                image.addClass('searchImage');
+                searchDiv.append(p);
+                searchDiv.append(image);
+                $('#searches').append(searchDiv);
+            }
+        });
+})
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).done(function (response) {
-    console.log(response);
-    console.log(response.rating);
-});
+$(document).on('click', '.searchImage', function () {
+    var state = $(this).data('state');
+    if (state === ' still') {
+        $(this).attr('src', $(this).data('animated'));
+        $(this).attr('data-state', 'animated');
+    } else {
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
+})
+
+$('#addSearch').on('click', function () {
+    var newSearch = $('input').eq(0).val();
+    searchArray.push(newSearch);
+    populateButtons(searchArray, 'searchButton', '#buttonsArea');
+    return false;
+})
